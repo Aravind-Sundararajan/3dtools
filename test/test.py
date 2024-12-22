cda6b14502a7ff9c1d191b272f93c6e79b212f0a
AspectWaltz <Aravind.Sundararajan@protonmail.com>
2024-12-22 17:28:59 -0500

    setting up executables

--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
-from py3dtools.vtp2stl import vtp2stl
-from py3dtools.stl2obj import stl2obj
+import py3dtools
 
+vtp2stl = py3dtools.get_vtp2stl()
+stl2obj = py3dtools.get_stl2obj()
 vtp2stl.convert_files('test', 'test')
 stl2obj.convert_files('test', 'test')
